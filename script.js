8f413d398fae9e1ab690d29595d20a7761c1d51e
lucasact4 <funcraftact4@outlook.com>
2024-04-23 20:38:57 -0300

    added reflection effect and more confetti

--- script.js
+++ script.js
@@ -61,3 +61,71 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const divIconContainer = document.querySelector('.glass-shine-effect-profile');
+
+    divIconContainer.addEventListener('click', function(event) {
+      dispararEmojisNoProfile(event.currentTarget);
+    });
+});
+
+function dispararEmojisNoProfile(elementoClicado) {
+  const rect = elementoClicado.getBoundingClientRect();
+
+  const x = rect.left + (rect.width / 2);
+  const y = rect.top + (rect.width / 2);
+
+  var scalar = 2;
+  var unicorn = confetti.shapeFromText({ 
+    text: '</>', 
+    scalar,
+    font: 'bold 32px Arial', 
+    color: 'black', 
+    opacity: 1,
+    shadow: {
+        color: 'black',
+        blur: 10,
+        offsetX: 2,
+        offsetY: 2
+    }
+  });
+  
+  var defaults = {
+    spread: 360,
+    ticks: 60,
+    gravity: 0,
+    decay: 0.96,
+    startVelocity: 20,
+    shapes: [unicorn],
+    scalar
+  };
+  
+  function shoot() {
+    confetti({
+      ...defaults,
+      particleCount: 30,
+      origin: { x: x / window.innerWidth, y: y / window.innerHeight }
+    });
+  
+    confetti({
+      ...defaults,
+      particleCount: 5,
+      flat: true,
+      origin: { x: x / window.innerWidth, y: y / window.innerHeight }
+    });
+  
+    confetti({
+      ...defaults,
+      particleCount: 15,
+      scalar: scalar / 2,
+      shapes: ['circle'],
+      origin: { x: x / window.innerWidth, y: y / window.innerHeight }
+    });
+  }
+  
+  setTimeout(shoot, 0);
+  setTimeout(shoot, 100);
+  setTimeout(shoot, 200);
+}
